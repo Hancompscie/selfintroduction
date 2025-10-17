f2e413bf383c39a8f587184ddbefe7e0ac917455
Muhammad Raihansyah Zahari <mraihansyah685@gmail.com>
2025-10-17 12:16:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-nama = 'raihan'
-usia = 15
-tinggi_badan = 180
 
-print(f"Halo, nama saya {nama}, usia saya adalah {usia}")
-print(f"Tinggi badan saya adalah {tinggi_badan} cm")
+nama = "Muhammad Raihansyah Zahari"
+usia = 15
+sekolah = "SMA Negeri 21 Bekasi"
+universitas_impian = "Universitas Indonesia"
+fakultas = "Ilmu Komputer"
+prodi = "Sistem Informasi"
+goals_pekerjaan = "Software Engineer atau Machine Learning Engineer di perusahaan besar luar negeri"
+
+
+print(f"Halo, nama saya {nama}, saya berusia {usia} tahun.")
+print(f"Saya mengenyam pendidikan di {sekolah}.")
+print(f"Impian terbesar saya adalah melanjutkan pendidikan di {universitas_impian}, Fakultas {fakultas}, program studi {prodi}.")
+print(f"Goals pekerjaan saya adalah menjadi {goals_pekerjaan}.")
