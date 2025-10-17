98b6fd068b68db6929705ca053b7c6b4539c51cd
Muhammad Raihansyah Zahari <mraihansyah685@gmail.com>
2025-10-17 12:25:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-
 nama = "Muhammad Raihansyah Zahari"
 usia = 15
 sekolah = "SMA Negeri 21 Bekasi"
 universitas_impian = "Universitas Indonesia"
 fakultas = "Ilmu Komputer"
 prodi = "Sistem Informasi"
-goals_pekerjaan = "Software Engineer atau Machine Learning Engineer di perusahaan besar luar negeri"
+goals_pekerjaan = "Software Engineer di Microsoft dan Google, United States of America"
 
-
-print(f"Halo, nama saya {nama}, saya berusia {usia} tahun.")
-print(f"Saya mengenyam pendidikan di {sekolah}.")
-print(f"Impian terbesar saya adalah melanjutkan pendidikan di {universitas_impian}, Fakultas {fakultas}, program studi {prodi}.")
-print(f"Goals pekerjaan saya adalah menjadi {goals_pekerjaan}.")
+print(
+    f"Halo nama saya {nama}, usia saya {usia} tahun, "
+    f"saya mengenyam pendidikan di {sekolah}, dan impian terbesar saya adalah "
+    f"bisa melanjutkan pendidikan di {universitas_impian}, fakultas {fakultas} "
+    f"prodi {prodi}. Dan goals pekerjaan saya adalah menjadi {goals_pekerjaan}."
+)
